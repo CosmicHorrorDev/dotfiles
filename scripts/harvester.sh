ccb4f381d967c963488c4e8ce30297c79a048e30
LovecraftianHorror <LovecraftianHorror@pm.me>
2018-06-21 18:56:37 -0400

    Added More Flexibility and Configs

--- scripts/harvester.sh
+++ scripts/harvester.sh
@@ -1,43 +1,49 @@
 #! /bin/bash
 
 # Lay some groundwork
-REPO_HOME=~/Programming/Repos/dotfiles
+DOTS_HOME=~/Programming/Repos/dotfiles/dotfiles
+EXTRA_HOME=~/Programming/Repos/dotfiles/dots_with_extra_steps
 CONFIG_HOME=~/.config
 
 # clean house
-rm -ri $REPO_HOME/dotfiles
+rm -ri $DOTS_HOME/*
+rm -ri $EXTRA_HOME/*
 
 # zsh configs
-mkdir -p $REPO_HOME/dotfiles/zsh
-cp $CONFIG_HOME/zsh/.zprofile $REPO_HOME/dotfiles/zsh/.zprofile
-cp $CONFIG_HOME/zsh/.zshenv $REPO_HOME/dotfiles/zsh/.zshenv
-cp $CONFIG_HOME/zsh/.zshrc $REPO_HOME/dotfiles/zsh/.zshrc
+mkdir $DOTS_HOME/zsh
+cp $CONFIG_HOME/zsh/.zprofile $DOTS_HOME/zsh/.zprofile
+cp $CONFIG_HOME/zsh/.zshenv $DOTS_HOME/zsh/.zshenv
+cp $CONFIG_HOME/zsh/.zshrc $DOTS_HOME/zsh/.zshrc
 
 # rofi configs - whole directory
-cp -r $CONFIG_HOME/rofi $REPO_HOME/dotfiles
+cp -r $CONFIG_HOME/rofi $DOTS_HOME
 
 # ranger configs
-mkdir -p $REPO_HOME/dotfiles/ranger
-cp $CONFIG_HOME/ranger/rc.conf $REPO_HOME/dotfiles/ranger/rc.conf
-cp $CONFIG_HOME/ranger/rifle.conf $REPO_HOME/dotfiles/ranger/rifle.conf
+mkdir $DOTS_HOME/ranger
+cp $CONFIG_HOME/ranger/rc.conf $DOTS_HOME/ranger/rc.conf
+cp $CONFIG_HOME/ranger/rifle.conf $DOTS_HOME/ranger/rifle.conf
 
 # polybar setup - whole directory
-cp -r $CONFIG_HOME/polybar $REPO_HOME/dotfiles
+cp -r $CONFIG_HOME/polybar $DOTS_HOME
 
 # neovim config - whole directory
-cp -r $CONFIG_HOME/nvim $REPO_HOME/dotfiles
+cp -r $CONFIG_HOME/nvim $DOTS_HOME
 
 # neofetch config - whole directory
-cp -r $CONFIG_HOME/neofetch $REPO_HOME/dotfiles
+cp -r $CONFIG_HOME/neofetch $DOTS_HOME
 
 # i3 config - whole directory
-cp -r $CONFIG_HOME/i3 $REPO_HOME/dotfiles
+cp -r $CONFIG_HOME/i3 $DOTS_HOME
 
 # htop config - whole directory even though it's volatile for a config
-cp -r $CONFIG_HOME/htop $REPO_HOME/dotfiles
+cp -r $CONFIG_HOME/htop $DOTS_HOME
 
 # git config
-cp $HOME/.gitconfig $REPO_HOME/dotfiles/.gitconfig
+cp $HOME/.gitconfig $DOTS_HOME/.gitconfig
 
 # Xdefaults config - should move to Xresources though
-cp $HOME/.Xdefaults $REPO_HOME/dotfiles/.Xdefaults
+cp $HOME/.Xdefaults $DOTS_HOME/.Xdefaults
+
+# lesskey file, takes extra to get it set up though
+mkdir $EXTRA_HOME/less
+cp $CONFIG_HOME/less/lesskey $EXTRA_HOME/less
