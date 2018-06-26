cc2d70335c8ec6996a47b6568f75c647e4b74658
LovecraftianHorror <LovecraftianHorror@pm.me>
2018-06-25 21:07:11 -0400

    Increased Locker Edginess

--- custom_scripts/lock.sh
+++ custom_scripts/lock.sh
@@ -5,44 +5,47 @@ C='#ffffff00'  # clearish
 D='#4f97d7cc'  # default
 W='#f22272bb'  # wrong
 
-i3lock                   \
+i3lock                        \
 \
---line-uses-inside       \
---insidecolor=$B         \
---ringcolor=$D           \
---separatorcolor=$W      \
+--line-uses-inside            \
+--insidecolor=$B              \
+--ringcolor=$D                \
+--separatorcolor=$W           \
 \
---keyhlcolor=$W          \
---bshlcolor=$W           \
+--keyhlcolor=$W               \
+--bshlcolor=$W                \
 \
---blur 5                 \
---ignore-empty-password  \
---clock                  \
---indicator              \
+--blur 5                      \
+--ignore-empty-password       \
+--clock                       \
+--indicator                   \
+--noinputtext=""              \
 \
---timecolor=$D           \
---timestr="%H:%M"        \
---time-font=Iosevka:bold \
---timesize=30            \
+--timecolor=$D                \
+--timestr="%H %M"             \
+--time-font=Iosevka:bold      \
+--timesize=40                 \
+--timepos="ix:iy - 10"        \
 \
---datecolor=$D           \
---datestr="%m:%d" \
---datesize=18            \
---date-font=Iosevka:bold \
---datesize=30            \
+--datecolor=$D                \
+--datestr="%m %d"             \
+--datesize=18                 \
+--date-font=Iosevka:bold      \
+--datesize=40                 \
+--datepos="ix:iy + 45"        \
 \
---verifcolor=$D          \
---ringvercolor=$D        \
---insidevercolor=$C      \
---veriftext="検証する"   \
---verif-font=IPAPGothic  \
---verifsize=40           \
---verifpos="ix:iy + 15"  \
+--verifcolor=$D               \
+--ringvercolor=$D             \
+--insidevercolor=$C           \
+--veriftext="検証する"        \
+--verif-font=IPAPGothic:bold  \
+--verifsize=40                \
+--verifpos="ix:iy + 15"       \
 \
---wrongcolor=$W          \
---ringwrongcolor=$W      \
---insidewrongcolor=$C    \
---wrongtext="馬鹿"       \
---wrong-font=IPAPGothic  \
---wrongsize=40           \
---wrongpos="ix:iy + 15"  \
+--wrongcolor=$W               \
+--ringwrongcolor=$W           \
+--insidewrongcolor=$C         \
+--wrongtext="馬鹿"            \
+--wrong-font=IPAPGothic:bold  \
+--wrongsize=40                \
+--wrongpos="ix:iy + 15"       \
